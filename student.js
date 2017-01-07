f2845d78154299ddd12522763d0a40e5de0dbe2c
dhirajshahninja <dhirajshahninja@gmail.com>
2017-01-07 11:43:13 +0530

    changes from feature/student01 for test the line conflict or not

--- student.js
+++ student.js
@@ -5,4 +5,6 @@ console.log('this line add from feature/student01');
 
 console.log('now i will change from     feature/student01');
 console.log('now i will change from feature/student01');
-console.log('this line add from feature/student01');
+console.log('this line add from feature/student01 no one data is changes in this line so i am changes here for tesing line conflict or not');
+
+
