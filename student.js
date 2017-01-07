a9e59a02fff1c6b70b019e4b646405bebcccbccb
dhirajshahninja <dhirajshahninja@gmail.com>
2017-01-07 11:59:17 +0530

    this commit only check for line conflict test

--- student.js
+++ student.js
@@ -4,7 +4,7 @@ console.log('this line change from feature/config01 for checking the conflict wi
 console.log('this line add from feature/student01');
 
 console.log('now i will change from     feature/student01');
-console.log('now i will change from feature/student01');
-console.log('this line add from feature/student01 no one data is changes in this line so i am changes here for tesing line conflict or not');
+console.log('now i "now i am here for conflict" will change from feature/student01');
+console.log('this line add from feature/student01 no one "now you can  "conflict test accures or not" check conflict here for testing" data is changes in this line so i am changes here for tesing line conflict or not');
 
 
